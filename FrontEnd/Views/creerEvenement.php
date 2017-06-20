2327c1ac6da8af573bf724d04539917d6903096e
Simclem <simon.clement98@yahoo.com>
2017-06-20 11:48:03 +0200

    Ajout du formulaire pour l'ajout d'un évènement

---
+++ FrontEnd/Views/creerEvenement.php
@@ -0,0 +1,34 @@
+<?php
+    include ("./Components/header.php");
+?>
+<div class="col-md-offset-5 col-md-2">
+    <label for="Nom">Nom de l'évènement</label>
+    <input type="text" class="form-control" id="Nom" placeholder="Entrer le nom de l'évènement" required="required"><br>
+</div>
+<div class="col-md-offset-5 col-md-2">
+    <label for="Desc">Description</label>
+    <input type="text-area" class="form-control" id="Desc" placeholder="Entrer votre description d'évenement" required="required"><br>
+</div>
+<div class="col-md-offset-5 col-md-2">
+    <label for="DateEvent">Date</label>
+    <input type="date" class="form-control" id="DateEvent" placeholder="Entrer votre adresse e-mail" required="required"><br>
+</div>
+<div class="col-md-offset-5 col-md-2">
+    <label for="HeureEvent">Heure</label>
+    <input type="text" class="form-control" id="HeureEvent" placeholder="Entrer l'heure du début" required="required"><br>
+</div>
+<div class="col-md-offset-5 col-md-2">
+    <label for="Duree">Durée</label>
+    <input type="text" class="form-control" id="Duree" placeholder="Entrer la durée de votre évènement"required="required"><br>
+</div>
+  <div class="col-md-offset-5 col-md-2">
+      <label for="urlPhoto">URL de la photo</label>
+      <input type="text" class="form-control" id="urlPhoto" placeholder="URL de la photo"required="required"><br>
+  </div>
+<div class="col-md-offset-5 col-md-2">
+    <button type="submit" class="btn btn-primary" id="submit" onclick="insertUser()">S'inscrire</button>
+</div>
+
+<?php
+    include ("./Components/footer.php");
+?>
