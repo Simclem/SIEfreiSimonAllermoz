8c116719892efef26be6bca2f53c4255cbf4f778
iFeYZenX <iFeYZenX@users.noreply.github.com>
2017-06-20 14:09:09 +0200

    ajout du reglement

--- FrontEnd/Views/reglement.php
+++ FrontEnd/Views/reglement.php
@@ -63,9 +63,153 @@
                 <b>ARTICLE 9</b></br>
                 Le club décline toute responsabilité en cas de perte ou de vol sur les courts et dans les parties communes.
             </p>
-            <br>
+            <p>
                 <b>ARTICLE 10</b></br>
                 L'adhésion au club entraîne l'acceptation de toutes les clauses du présent règlement.
+            </p>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <h3 style="text-align: center"><b> Statuts </b></h3>
+            </br>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="container-fluid">
+        <div class="col-md-offset-1 col-md-10">
+            <p>
+                <b>TITRE I - OBJET - DÉNOMINATION - SIÈGE – DURÉE</b></br>
+            </p>
+            <p>
+                <b>ARTICLE 1 - Objet</b></br>
+                Il est formé, entre les soussignés et les personnes qui adhéreront aux présentes et rempliront les conditions ci-après, une association qui est régie par la loi du 1er juillet 1901. Elle a pour objet la pratique du tennis.
+            </p>
+            <p>
+                <b>ARTICLE 2 - Dénomination</b></br>
+                La dénomination de l'association est TCB.
+            </p>
+            <p>
+                <b>ARTICLE 3 - Durée</b></br>
+                La durée de l'association est illimitée.
+            </p>
+            <p>
+                <b>ARTICLE 4 - Lieu</b></br>
+                Le siège de l'association est à la Mairie de Bruyères le Châtel. Il peut être transféré à tout autre endroit de la même ville par décision du Comité de direction et ratification de l'Assemblée générale, dans une autre localité par délibération de l'Assemblée générale extraordinaire.
+            </p>
+            <p>
+                <b>ARTICLE 5 - Moyens d'action</b></br>
+                Les moyens d'action de l'association sont notamment : l'organisation de toutes les épreuves, compétitions ou manifestations sportives entrant dans le cadre de son activité, et en général toutes initiatives propres à servir cette activité.
+            </p>
+            <p>
+                <b>TITRE II - COMPOSITION DE L'ASSOCIATION</b></br>
+            </p>
+            <p>
+                <b>ARTICLE 6 - Les membres L'association se compose de membres</b></br>
+                Chaque membre de l'association doit payer une cotisation annuelle qui est fixée par l'Assemblée générale. L'admission d'un membre comporte de plein droit par ce dernier, adhésion aux statuts et règlements intérieurs. Pour être membre de l'association, il faut avoir acquitté la cotisation fixée par l’assemblée générale et être détenteur d'une licence fédérale de l'année en cours. Les membres ont seuls le droit de prendre part aux réunions sportives organisées par l'association, par la Fédération et la Ligue de tennis à laquelle l'association sera affiliée et par les associations affiliées à cette Fédération.
+            </p>
+            <p>
+                <b>ARTICLE 7 - Perte de la qualité de membre</b></br>
+                La qualité de membre de l'association se perd :</br>
+                1. Par la démission, par lettre adressée au Président de l'association, ou par le décès.</br>
+                2. Par la radiation prononcée par le Comité de direction pour non-paiement de la cotisation ou pour motifs graves, l'intéressé ayant préalablement été appelé à fournir des explications, sauf recours à l'Assemblée générale. La radiation d’un membre peut être prononcée avec extension à toutes les associations affiliées à la fédération de tennis.</br>
+                3. Par la radiation prononcée selon les règlements de la Fédération Française de Tennis. Les membres démissionnaires ou exclus ne seront pas remboursés de leur cotisation. Le décès, la démission, ou l’exclusion d’un membre de l’association n’entraîne pas la dissolution de cette dernière qui continue d’exister entre les autres membres de l’association.
+            </p>
+            <p>
+                <b>ARTICLE 8 - Rétribution des membres</b></br>
+                Les membres du Comité ne peuvent recevoir aucune rétribution en raison des fonctions qu'ils exercent.
+            </p>
+            <p>
+                <b>ARTICLE 9 - L'actif de l'association</b></br>
+                L'actif de l'association répond seul des engagements contractés en son nom, sans qu'aucun des membres de l'association ou du Comité ne puisse en être personnellement responsable.</br>
+                Les membres de l'association qui cesseront d'en faire partie pour une cause quelconque n'ont aucun droit sur l'actif de l'association, celle-ci se trouvant entièrement dégagée vis-à-vis d'eux.
+            </p>
+            <p>
+                <b>ARTICLE 10 - Les devoirs de l'association</b></br>
+                L’association s’engage :</br>
+                1. À se conformer entièrement aux règlements établis par la Fédération Française de Tennis ou par ses ligues.</br>
+                2. À exiger de tous les membres qu'ils soient détenteurs de la licence fédérale de l'année en cours.</br>
+                3. À se soumettre aux sanctions disciplinaires qui lui seraient infligées par application desdits règlements.</br>
+                4. À s’interdire toute discrimination illégale dans l’organisation et la vie de l’association.</br>
+                5. À veiller à l’observation des règles déontologiques du sport définies par le Comité national olympique et sportif français.</br>
+                6. À respecter les règles d’encadrement, d’hygiène et de sécurité applicables aux disciplines sportives pratiquées par leurs membres.</br>
+                7. À tenir à jour une liste nominative de ses membres indiquant pour chacun d'eux le numéro de la licence délivrée par la Fédération Française de Tennis.</br>
+                8. À verser à la Fédération Française suivant les modalités fixées par les règlements de celle-ci toute somme dont le paiement est prévue par lesdits règlements. En cas de partage, la voix du Président est prépondérante. Les délibérations du Comité sont constatées par des procès-verbaux signés par le Président de la séance et par le Secrétaire. Ces documents font l’objet d’un archivage électronique. présents et le cas échéant représentés.
+            </p>
+            <p>
+                <b>TITRE III - RESSOURCES DE L'ASSOCIATION (1)</b></br>
+            </p>
+            <p>
+                <b>ARTICLE 11 - Ressources</b></br>
+                Les ressources annuelles de l'association se composent :</br>
+                1. Des cotisations versées par ses membres dans les termes de la loi.</br>
+                2. Des subventions qui peuvent lui être accordées.</br>
+                3. Des revenus de biens et valeurs appartenant à l'association.</br>
+                4. Des recettes des manifestations sportives.</br>
+                5. Des recettes des manifestations non sportives organisées à titre exceptionnel.</br>
+                6. De toutes autres ressources ou subventions qui ne seraient pas contraires aux lois en vigueur.</br>
+                Il est tenu une comptabilité complète de toutes les recettes et de toutes les dépenses. Le budget annuel est adopté par le conseil d'administration avant le début de l'exercice. Les comptes sont soumis à l'Assemblée générale dans un délai inférieur à six mois à compter de la clôture de l'exercice. Est éligible au Comité de Direction tout électeur. Ne peuvent être sortant simultanément qu’au plus la moitié des membres du Comité de Direction.</br>
+                (1) Seules des associations reconnues d'utilité publique peuvent recevoir des dons et legs.
+            </p>
+            <p>
+                <b>TITRE IV - ADMINISTRATION</b></br>
+            </p>
+            <p>
+                <b>ARTICLE 12 - Election du Comité de direction</b></br>
+                L’association est administrée par un Comité de direction composé de membres (5 au moins) élus, ces membres sont élus par l’Assemblée générale ordinaire pour une durée de 3 années entières et consécutives, à la majorité relative des membres présents. Peuvent seuls prendre part à l’élection des membres du Comité de Direction, les membres âgés de 16 ans au moins.</br>
+                Est éligible au Comité de Direction tout électeur. Ne peuvent être sortant simultanément qu’au plus la moitié des membres du Comité de Direction.
+            </p>
+            <p>
+                <b>ARTICLE 13 - Election du Bureau</b></br>
+                Le Comité de direction élit chaque année son Bureau qui est composé d'au moins un Président, un Secrétaire, un Trésorier.
+            </p>
+            <p>
+                <b>ARTICLE 14 - Les réunions</b></br>
+                Le Comité se réunit au moins deux fois par an et sur la convocation de son Président ou à la demande de la majorité des membres qui le composent. Le Bureau se réunit en principe une fois par mois sur convocation du Président ou à la demande de la majorité des membres qui le composent. La présence de la majorité au moins des membres est nécessaire pour la validité des décisions qui sont prises à la majorité des membres présents.
+            </p>
+            <p>
+                <b>ARTICLE 15 - Rôles du Comité de direction</b></br>
+                Le Comité est investi des pouvoirs les plus étendus pour la gestion et la direction des affaires de l'association et pour faire autoriser tous actes et opérations permis à l'association et qui ne sont pas réservés à l'Assemblée générale. Il délibère et statue notamment sur toutes les propositions qui lui sont présentées, sur l'attribution des recettes, sur les demandes d'admission, de congé et sur les radiations. Le Bureau du Comité de direction expédie toutes les affaires urgentes dans l'intervalle des séances du comité de direction. Il est spécialement chargé de l'administration courante de l'association et de ses différents services, des rapports avec les pouvoirs publics et la Fédération Française de Tennis. Il prend d'urgence toutes mesures nécessaires au bien de l'Association et du sport, sous condition d'en référer au Comité de direction à sa première réunion.
+            </p>
+            <p>
+                <b>ARTICLE 16 - Rôle des membres du Bureau</b></br>
+                Le Président est chargé d'exécuter les décisions du Comité de direction et du Bureau. À l’exception des paiements automatiques il signe avec le Trésorier les ordonnances de paiement, les retraits et décharges de sommes, les actes de vente et d'achat de tous titres et valeurs et toutes opérations de caisse. Il préside les Assemblées générales et les réunions. Il représente l'association en justice et dans tous les actes de la vie civile. Le Secrétaire rédige les procès-verbaux et la correspondance, tient le registre des membres de l'association et garde les archives. Le trésorier est dépositaire des fonds de l'association, tient le livre de recettes et de dépenses, encaisse les cotisations, droits d'entrée, dons, etc...
+            </p>
+            <p>
+                <b>ARTICLE 17 - Rôle des autres membres du Comité</b></br>
+                Les attributions des autres membres du Comité sont déterminées par un règlement intérieur, arrêté par le Comité et approuvé par l'Assemblée générale ordinaire.
+            </p>
+            <p>
+                <b>ARTICLE 18 - Assemblées</b></br>
+                Les Assemblées générales, tant ordinaires qu'extraordinaires, se composent des membres de l'association. Elles se réunissent aux jours, heures et lieux indiqués dans l'avis de convocation adressé par le Comité.
+            </p>
+            <p>
+                <b>TITRE V - LES ASSEMBLÉES GÉNÉRALES</b></br>
+            </p>
+            <p>
+                <b>ARTICLE 19</b></br>
+                Les convocations sont faites quinze jours au moins à l'avance par lettre adressée à chacun des sociétaires en indiquant l'objet de la réunion. L'ordre du jour est arrêté par le Comité.
+            </p>
+            <p>
+                <b>ARTICLE 20</b></br>
+                L'Assemblée est présidée par le Président du Comité ou à défaut par un membre du Comité désigné par celui-ci. Il est dressé une feuille de présence signée par les membres de l'assemblée en entrant en séance et certifiée par le Président et le Secrétaire. Nul ne peut représenter un membre s'il n'est lui-même membre de l'Assemblée.
+            </p>
+            <p>
+                <b>ARTICLE 21</b></br>
+                Chaque membre de l'Assemblée a une voix et autant de voix supplémentaires qu'il a de procurations qui lui ont été données par les membres n'assistant pas à l'Assemblée.</br>
+                Le vote par procuration peut être admis statutairement mais le vote par correspondance n’est pas admis. Le nombre de procurations possibles par votant est fixé à 3.
+            </p>
+            <p>
+                <b>ARTICLE 22 - L'Assemblée générale ordinaire</b></br>
+                L'Assemblée générale ordinaire se réunit une fois par an et en cas de nécessité sur convocation extraordinaire, sur proposition du Comité de direction ou à la demande du dixième des membres dont se compose l'Assemblée générale.</br>
+                Elle approuve ou redresse les comptes de l'exercice clos, vote le budget de l'exercice suivant et, d'une manière générale, délibère sur toutes les questions d'intérêt général et sur toutes celles qui lui sont soumises par le Comité. L'Assemblée générale ordinaire, pour être tenue valablement, doit se composer du quart au moins des membres ayant le droit d'en faire partie : si cette proportion n'est pas atteinte, l'Assemblée est convoquée à nouveau à quinze jours au moins d'intervalle et cette fois, elle délibère valablement quel que soit le nombre des membres présents ou représentés lorsque le vote par procuration est permis. Elle procède à l'élection des membres du Comité de direction qui désigne des représentants de l'Association à la Ligue de la Fédération Française de Tennis dont dépend l’association. Les décisions sont prises à la majorité absolue des voix des membres présents et, le cas échéant représentés.
+            </p>
+            <p>
+                <b>ARTICLE 23 - L'Assemblée générale extraordinaire</b></br>
+                L'Assemblée générale extraordinaire se compose des membres de l'association. Elle peut modifier les statuts dans toutes leurs dispositions sur la proposition du Comité de direction ou sur celle du dixième des membres dont se compose l'Assemblée générale soumise au Comité de direction au moins un mois avant la réunion de l'Assemblée. Elle peut décider notamment la dissolution anticipée ou la prorogation de l'association, sa fusion ou son union avec d'autres associations du même genre ayant le même objet. L'Assemblée générale extraordinaire, pour être tenue valablement, doit se composer de la moitié au moins des membres ayant le droit d'en faire partie. Si cette proportion n'est pas atteinte, l'assemblée est convoquée à nouveau à quinze jours au moins d'intervalle et cette fois elle délibère valablement quel que soit le nombre des membres présents ou représentés mais seulement sur les questions à l'ordre du jour de la précédente Assemblée. Les délibérations de l'Assemblée générale extraordinaire quel qu'en soit le quorum, sont prises à la majorité des deux tiers des voix des membres.
             </p>
         </div>
     </div>
