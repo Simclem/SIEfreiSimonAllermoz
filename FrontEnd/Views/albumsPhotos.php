ea403e3b39e3392743b1a27e0b6c9a43b17f6ee1
Simclem <simon.clement98@yahoo.com>
2017-07-25 22:04:44 +0200

    Suppression du en construction

--- FrontEnd/Views/albumsPhotos.php
+++ FrontEnd/Views/albumsPhotos.php
@@ -63,14 +63,6 @@
 
 
 
-<div class="row">
-    <div class="container-fluid">
-        <div class="col-md-12">
-            <h3 style="text-align: center"><b> En Construction ... </b></h3>
-            </br></br>
-        </div>
-    </div>
-</div>
 
 <?php
     include ("./Components/footer.php");
