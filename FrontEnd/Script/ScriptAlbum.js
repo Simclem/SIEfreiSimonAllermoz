63cbf36b96c2509034ebb411d85dbdf5d5446237
Simclem <simon.clement98@yahoo.com>
2017-07-25 22:03:18 +0200

    Ajout de la page permettant de voir l'ensemble des albums

--- FrontEnd/Script/ScriptAlbum.js
+++ FrontEnd/Script/ScriptAlbum.js
@@ -6,7 +6,26 @@ window.onload = function setAllAlbums()
     dataType:'json',
     success: function(data)
     {
-      console.log(data)
+      var line = document.getElementsByTagName('tbody')[0];
+      var indiceTD = 2;
+      for (i = 0 ; i < data.length; i ++)
+      {
+        newLine = document.createElement('tr');
+        line.appendChild(newLine);
+
+        var currentLine=document.getElementsByTagName('tr')[i+1];
+        newCell = document.createElement('td');
+        newCell.style.border = "none";
+        currentLine.appendChild(newCell);
+        document.getElementsByTagName('td') [indiceTD].innerHTML = data[i].idAlbum
+        indiceTD+=1;
+        var currentLine=document.getElementsByTagName('tr')[i+1];
+        newCell = document.createElement('td');
+        newCell.style.border = "none";
+        currentLine.appendChild(newCell);
+        document.getElementsByTagName('td') [indiceTD].innerHTML = "<a href=photo.php?idAlbum="+data[i].idAlbum+">" + data[i].titre + "</a>"
+        indiceTD+=1;
+      }
     }
   })
 }
--- FrontEnd/Views/albumsPhotos.php
+++ FrontEnd/Views/albumsPhotos.php
@@ -1,6 +1,67 @@
 <?php
     include ("./Components/header.php");
 ?>
+<script type = "text/javascript" src="../Script/ScriptAlbum.js"></script>
+<div class="row">
+  <div class="container-fluid">
+      <div class="col-md-offset-4 col-md-4">
+          <div class="alert alert-success" role="alert" id="displayConnect" style="display: none; text-align: center">Vous êtes inscrit à l'évenement.</div>
+          <div class="alert alert-danger" role="alert" id="displayError" style="display: none; text-align: center">Vous n'êtes pas connectés, impossible de vous inscrire.</div>
+      </div>
+  </div>
+    <div class="container-fluid">
+
+        <div class="col-md-offset-2 col-md-8">
+            <h3 style="text-align: center"><b> Les Albums </b></h3>
+            </br></br></br>
+        </div>
+        <?php
+          if (!empty($_SESSION["isAdmin"]))
+          {
+            if ($_SESSION["isAdmin"] == 1){
+                ?>
+                <div class="col-md-2" style="text-align: center">
+                    <a class="btnCreateEvent" href="creerEvenement.php"><button  type="button" class="btn btn-primary"> Créer un album </button></a></br></br>
+                  <!--  <a class="btnCreateEvent" href="modifierEvenement.php"><button type="button" class="btn btn-primary"> Modifier un évènement </button></a>-->
+                </div>
+                <?php
+            }
+          }
+
+        ?>
+    </div>
+</div>
+
+<div class="row">
+  <div class="container-fluid">
+    <div class="col-md-offset-1 col-md-10">
+      <div class="panel panel-default">
+      <div class="panel-body">
+
+    <table class="table">
+      <tr>
+        <td style="border-top:  none;">
+          Numéro de l'album
+        </td>
+        <td style="border-top:  none;">
+          Titre
+        </td>
+      </tr>
+    </table>
+  </div>
+  </div>
+</div>
+
+
+
+
+
+</div>
+</div>
+
+
+
+
 
 <div class="row">
     <div class="container-fluid">
