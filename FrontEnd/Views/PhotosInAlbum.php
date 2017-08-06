d136303417670bf33d699734c225c0f3847ff418
Simclem <simon.clement98@yahoo.com>
2017-08-06 10:08:06 +0200

    Ajout du bouton permettant d'ajouter des photo à un album si on est admin

--- FrontEnd/Views/PhotosInAlbum.php
+++ FrontEnd/Views/PhotosInAlbum.php
@@ -2,8 +2,24 @@
     include('./Components/header.php');
     echo('<input type="hidden" id="idA" value ='.$_GET["idAlbum"].'>');
 ?>
+<?php
+          if (!empty($_SESSION["isAdmin"]))
+          {
+            if ($_SESSION["isAdmin"] == 1){
+                ?>
+                <div class="row">
+                    <div class="col-md-2 col-md-offset-10" style="text-align: center">
+                        <?php echo('<a class="btnCreateEvent" href="ajoutPhoto.php?idAlbum='.$_GET["idAlbum"].'"><button  type="button" class="btn btn-primary"> Ajouter une photo à l\'album </button></a></br></br>');?>
+                    </div>  
+                </div>
+                <?php
+            }
+          }
+
+        ?>
 <div class="gallery">
 </div>
+
 <script type = "text/javascript" src="../Script/ScriptGetPhotos.js"></script>
 
 
